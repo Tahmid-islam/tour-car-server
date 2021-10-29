40b95173ed4489c128291ae2d9f8148b4a129617
Tahmid Islam <tahmid231@gmail.com>
2021-10-29 23:49:54 +0600

    get post delete api done

--- index.js
+++ index.js
@@ -1,11 +1,69 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+var cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware configuration
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsljh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("tourCar");
+    const servicesCollection = database.collection("cars");
+
+    //GET API
+    app.get("/cars", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    //DELETE API
+    app.delete("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await servicesCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    //GET SINGLE SERVICE
+    app.get("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.json(service);
+    });
+
+    //POST API
+    app.post("/cars", async (req, res) => {
+      const services = req.body;
+      const result = await servicesCollection.insertOne(services);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Running Tour Car Server");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Running Tour Car Server on port`, port);
 });
